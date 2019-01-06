febba7e22b234f606c025757d7d739ce01e2eced
vpersie2008 <530689602@qq.com>
2019-01-06 22:55:47 +0800

    finished register

--- infrastructure/customer/customer.js
+++ infrastructure/customer/customer.js
@@ -1,4 +1,6 @@
 const CustomerDAL = require("./customerDAL");
+const bcrypt = require("bcrypt");
+const validator = require("./validator");
 
 class CustomerFacade {
 
@@ -8,11 +10,17 @@ class CustomerFacade {
             status: false
         };
 
+        const validateResult = validator.validateRegister(request);
+        if (!validateResult.status) {
+            return {message: validateResult.message, status: false};
+        }
+
         let isExist = await CustomerDAL.customerIsExist(request.loginName);
         if (isExist) {
             return {message: "该用户已被注册！", status: true};
         }
-        
+
+        request.password = bcrypt.hashSync(request.password, 10);
         const updateResult = await CustomerDAL.updateCustomer(request);
         if (updateResult.success) {
             return {message: "注册成功！", status: true};
---
+++ infrastructure/customer/validator.js
@@ -0,0 +1,53 @@
+const validator = require("validator");
+const validatorHelper = require("../utility/validation");
+
+let validateFailed = {
+    status: false,
+    message: "Request body is empty"
+}
+
+module.exports.validateRegister = (customer) => {
+
+    if (!customer) {
+        return validateFailed;
+    }
+
+    customer.loginName = !validatorHelper.isEmpty(customer.loginName)? customer.loginName: "";
+    customer.phone = !validatorHelper.isEmpty(customer.phone)? customer.phone: "";
+    customer.email = !validatorHelper.isEmpty(customer.email)? customer.email: "";
+    customer.password = !validatorHelper.isEmpty(customer.password)? customer.password: "";
+
+    let {loginName, phone, email, password} = customer;
+
+    if (validator.isEmpty(loginName)) {
+        return {status: false, message: "Login name is empty!"};
+    }
+
+    if (validator.isEmpty(phone)) {
+        return {status: false, message: "Cell phone is empty!"};
+    }
+
+    if (validator.isEmpty(password)) {
+        return {status: false, message: "Password is empty!"};
+    }
+
+    if (!validator.isLength(loginName, {
+        min: 3,
+        max: 30
+    })) {
+        return {status: false, message: "Login name is nust in 3~ 30!"};
+    }
+
+    if (!validator.isLength(password, {
+        min: 6,
+        max: 30
+    })) {
+        return {status: false, message: "Password is nust in 6~ 30!"};
+    }
+
+    if (!validator.isEmpty(email) && !validator.isEmail(email)) {
+        return {status: false, message: "Please input a valid email!"};
+    }
+
+    return {status: true, message: ""};
+}
